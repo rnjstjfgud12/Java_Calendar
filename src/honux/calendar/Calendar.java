30041fc4377b75b146ef95d0fe79d5ba95df612c
rnjstjfgud12 <61874872+rnjstjfgud12@users.noreply.github.com>
2020-05-19 14:25:24 +0900

    월을 입력하여 최대 일 수 출력

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -1,5 +1,5 @@
 package honux.calendar;
-
+import java.util.Scanner;
 public class Calendar {
 
 	public static void main(String[] args) {
@@ -9,5 +9,15 @@ public class Calendar {
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		
+		//입력받은 달의 최대 일 수 출력하기
+		
+		System.out.println("월을 입력하세요");
+		Scanner scanner = new Scanner(System.in);
+		int month = scanner.nextInt();
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		System.out.printf("%d월은 %d일까지 있습니다.", month, maxDays[month - 1]);
+		scanner.close();
 	}
 }
