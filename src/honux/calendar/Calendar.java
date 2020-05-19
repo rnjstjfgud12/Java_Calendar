cd4a105fb692346ec5d7e0e230ff7c3a8b4e9a36
rnjstjfgud12 <61874872+rnjstjfgud12@users.noreply.github.com>
2020-05-19 18:56:29 +0900

    요일별로 일 입력받는 로직 구성

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -1,69 +1,60 @@
 package honux.calendar;
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Calendar {
 	
 	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
-	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month - 1];
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
-	public void printSampleCalendar(int year, int month) {
+	public int getMaxDaysOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month - 1];
+		} else {
+			return MAX_DAYS[month - 1];
+		}
+	}
+	
+	public void printSampleCalendar(int year, int month, int weekday) {
 		System.out.printf("     <<%4d년%3d월>>     \n", year, month);
 		System.out.println(" 일    월   화   수  목   금   토");
 		System.out.println("--------------------");
 		
-		int maxDay = getMaxDaysOfMonth(month);
+		// print blank space
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+			
+		}
 		
-		for(int i = 1; i <= maxDay; i++) {
+		int maxDay = getMaxDaysOfMonth(year, month);
+		int count = 7 - weekday;
+		int delim = (count < 7) ? count : 0;
+		
+		// print first line
+		for (int i = 1; i <= count; i++) {
 			System.out.printf("%3d", i);
-			if (i % 7 == 0) {
+		}
+		System.out.println();
+		
+		//print from second line to last line
+		count++;
+		for(int i = count; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == delim) {
 				System.out.println();
 			}
 		}
 		System.out.println();
-//		System.out.println(" 1  2  3  4  5  6  7");
-//		System.out.println(" 8  9 10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 24 25 26 27 28");
+		System.out.println();
 	}
 	public static void main(String[] args) {
-		//입력받은 달의 최대 일 수 출력하기	
-//		Scanner scanner = new Scanner(System.in);
-//		Calendar cal = new Calendar();
-//		
-//		System.out.println("반복 횟수를 입력하세요");
-//		int repeat = scanner.nextInt();
-//		
-//		for (int i = 0; i < repeat; i++) {
-//			System.out.println("월을 입력하세요");
-//			int month = scanner.nextInt();
-//			System.out.printf("%d월은 %d일까지 있습니다.", month, cal.getMaxDaysOfMonth(month));
-//		}
-//		scanner.close();
 		
-		// 종료 조건을 이용해 반복하기
-		Scanner scanner = new Scanner(System.in);
-		String PROMPT="cal> ";
-		Calendar cal = new Calendar();
-		
-		int month = 1;
-		
-		while(month != -1) {
-			System.out.println("달을 입력하세요.");
-			System.out.print(PROMPT);
-			month = scanner.nextInt();
-			cal.printSampleCalendar(2020, month);
-			if(month == -1) {
-				break;
-			} else if(month > 12) {
-				System.out.println("없는 달입니다. 달을 다시 입력하세요");
-				continue;
-			}
-			System.out.printf("%d월은 %d일까지 있습니다.", month, cal.getMaxDaysOfMonth(month));
-		}
-		System.out.println("Finish");
-		scanner.close();
 	}
 }
--- src/honux/calendar/Prompt.java
+++ src/honux/calendar/Prompt.java
@@ -4,27 +4,58 @@ import java.util.Scanner;
 
 public class Prompt {
 	
-	private final static String PROMPT="cal> "; // 변경이 없을 경우 대문자로 보통 작성함
+	public int parseDay(String week) {
+		if (week.equals("일")) {
+			return 0;
+		} else if(week.equals("월")) {
+			return 1;
+		} else if(week.equals("화")) {
+			return 2;
+		} else if(week.equals("수")) {
+			return 3;
+		} else if(week.equals("목")) {
+			return 4;
+		} else if(week.equals("금")) {
+			return 5;
+		} else if(week.equals("토")) {
+			return 6;
+		} else {
+			return 0;
+		}
+	}
+	
+	// private final static String PROMPT="cal> "; // 변경이 없을 경우 대문자로 보통 작성함
 	
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
-		String PROMPT="cal> ";
+		// String PROMPT="cal> ";
 		Calendar cal = new Calendar();
 		
 		int month = 1;
+		int year = 2020;
+		int weekday = 0;
 		
 		while(month != -1) {
-			System.out.println("달을 입력하세요.");
-			System.out.print(PROMPT);
-			month = scanner.nextInt();
-			cal.printSampleCalendar(2020, month);
-			if(month == -1) {
+			System.out.println("년도를 입력하세요.(exit : -1)");
+			System.out.print("YEAR> ");
+			year = scanner.nextInt();
+			if(year == -1) {
 				break;
-			} else if(month > 12) {
-				System.out.println("없는 달입니다. 달을 다시 입력하세요");
+			}
+			
+			System.out.println("달을 입력하세요.");
+			System.out.print("MONTH> ");
+			month = scanner.nextInt();
+			System.out.println("첫 번째 요일을 입력하세요(일, 월, 화, 수, 목, 금, 토).");
+			String str_weekday = scanner.next();
+			weekday = parseDay(str_weekday);
+			
+			if(month > 12 || month < 1) {
+				System.out.println("잘못된 입력입니다.");
 				continue;
 			}
-			System.out.printf("%d월은 %d일까지 있습니다.", month, cal.getMaxDaysOfMonth(month));
+			
+			cal.printSampleCalendar(year, month, weekday);
 		}
 		System.out.println("Finish");
 		scanner.close();
